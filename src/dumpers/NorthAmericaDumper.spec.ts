b62a92c85a576d215dc44b879036331efb1c61af
Marcelo Paixão Resende <marcelothebuilder@users.noreply.github.com>
2020-03-31 10:07:31 -0300

    add more tests

--- src/dumpers/NorthAmericaDumper.spec.ts
+++ src/dumpers/NorthAmericaDumper.spec.ts
@@ -1,4 +1,5 @@
-import { expect } from "chai";
+/* eslint-disable no-unused-expressions */
+import { expect, assert } from "chai";
 import { describe, it, afterEach } from "mocha";
 import sinon from "sinon";
 import { SearchIndex } from "algoliasearch";
@@ -7,10 +8,47 @@ import { NorthAmericaDumper } from "./NorthAmericaDumper";
 describe("NorthAmericaDumper tests", () => {
   afterEach(() => sinon.restore());
 
-  it("NorthAmericaDumper should be created successfully", () => {
+  function getDumperWithIndex(indexStub: Partial<SearchIndex>) {
+    return new NorthAmericaDumper({ platform: "", algoliaIndex: indexStub as SearchIndex });
+  }
+
+  it("should be created successfully", () => {
     // eslint-disable-next-line no-new
-    const indexStub = {} as SearchIndex;
-    const instance = new NorthAmericaDumper({ platform: "", algoliaIndex: indexStub });
+    const instance = getDumperWithIndex({});
     expect(instance).to.be.instanceOf(NorthAmericaDumper);
+  });
+
+  describe("searchAll", () => {
+    describe("when algolia game list is empty", () => {
+      it("should return empty game list  result ", async () => {
+        const index = {
+          search: sinon.stub().resolves({
+            result: {
+              facets: [],
+            },
+          } as any),
+        };
+
+        const instance = getDumperWithIndex(index);
+
+        assert.deepEqual(await instance.searchAll(), { games: [] });
+      });
+
+      it.skip("should call index only once", async () => {
+        const fakeSearch = sinon.fake.resolves({
+          result: {
+            facets: [],
+          },
+        } as any);
+
+        const instance = getDumperWithIndex({
+          search: fakeSearch,
+        });
+
+        await instance.searchAll();
+
+        expect(fakeSearch.callCount).to.be.eq(1);
+      });
+    });
   });
 });
