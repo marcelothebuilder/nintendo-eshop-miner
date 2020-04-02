d5562a4f8bf42583e1623bc92cfac57840189ee8
Marcelo Paixão Resende <marcelothebuilder@users.noreply.github.com>
2020-04-02 17:14:41 -0300

    remove unused file

--- src/logging/assert.ts
+++
@@ -1,4 +0,0 @@
-import { logger } from "./logger";
-
-export const assert = (expression: () => boolean, expressionMessage: () => string) =>
-  !expression() ? logger.error(expressionMessage()) : null;
