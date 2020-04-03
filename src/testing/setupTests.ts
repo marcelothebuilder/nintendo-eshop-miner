5b4c3ecaaeb3b8a34610b0f0fde0ccdf500007f1
Marcelo Paixão Resende <marcelothebuilder@users.noreply.github.com>
2020-04-03 16:09:08 -0300

    adds single file testing through vscode

---
+++ src/testing/setupTests.ts
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* istanbul ignore file */
+import chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+
+chai.use(chaiAsPromised);
+chai.should();
