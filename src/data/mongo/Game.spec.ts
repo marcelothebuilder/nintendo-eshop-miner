431ac8d540cb893d735c2bb6f49f4187a8d97a15
Marcelo Paixão Resende <marcelothebuilder@users.noreply.github.com>
2020-04-10 20:39:05 -0300

    run/teardown of mongo models

--- src/data/mongo/Game.spec.ts
+++ src/data/mongo/Game.spec.ts
@@ -3,7 +3,6 @@ import { describe, it, afterEach } from "mocha";
 import sinon from "sinon";
 import chai, { expect } from "chai";
 import chaiAsPromised from "chai-as-promised";
-import mongoose from "mongoose";
 import { Game } from "./Game";
 
 chai.should();
@@ -16,7 +15,6 @@ const getGameDocument = () => {
 describe("Game", () => {
   afterEach(() => {
     sinon.restore();
-    mongoose.connection.deleteModel(/.+/);
   });
 
   it("should be created successfully", () => {
--- src/testing/setupTests.ts
+++ src/testing/setupTests.ts
@@ -3,7 +3,17 @@
 import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 import moxios from "moxios";
+import { after, before } from "mocha";
+import mongoose from "mongoose";
 
 chai.use(chaiAsPromised);
 chai.should();
 moxios.delay = 1;
+
+before(async () => {
+  mongoose.connection.deleteModel(/.+/);
+});
+
+after(async () => {
+  mongoose.connection.deleteModel(/.+/);
+});
