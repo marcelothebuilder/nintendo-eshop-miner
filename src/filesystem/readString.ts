758a8206b8bc3685191cf5789bac0e690ce16e8f
Marcelo Paixão Resende <marcelothebuilder@users.noreply.github.com>
2020-04-10 19:35:58 -0300

    remove unused code

--- src/filesystem/readString.ts
+++ src/filesystem/readString.ts
@@ -4,8 +4,3 @@ export const readString = async (path: PathLike, encoding = "utf-8"): Promise<st
   const content = await fs.promises.readFile(path, encoding);
   return content.toString();
 };
-
-export const readStringSync = (path: PathLike, encoding = "utf-8"): string => {
-  const content = fs.readFileSync(path, encoding);
-  return content.toString();
-};
