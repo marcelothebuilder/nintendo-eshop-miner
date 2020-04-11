77e31a500689fa74bca87572ade754a8c7f116df
Marcelo Paixão Resende <marcelothebuilder@users.noreply.github.com>
2020-04-11 14:06:48 -0300

    adds duplicate validation

--- src/data/mongo/Game.integration.ts
+++ src/data/mongo/Game.integration.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 import { expect } from "chai";
 import { describe, it, afterEach, before, after, beforeEach } from "mocha";
 import sinon from "sinon";
@@ -53,17 +54,29 @@ describe("Game", () => {
     await new Game({
       nsuid: 21311,
       name: "Zeldinha",
+      prices: [],
     }).save();
 
     await new Game({
       nsuid: 13131,
       name: "Zeldinha2",
+      prices: [],
     }).save();
 
     const games = await Game.find().lean().exec();
 
     expect(games.shift()?.nsuid).to.eq(21311);
     expect(games.shift()?.nsuid).to.eq(13131);
+  });
+
+  it("should saveAndFind", async () => {
+    const g = await new Game({
+      nsuid: 21311,
+      name: "Zeldinha",
+      prices: [],
+    }).saveAndFind();
+
+    expect(g).to.be.instanceOf(Game);
   });
 
   it("should allow two games with same name", async () => {
@@ -95,5 +108,168 @@ describe("Game", () => {
     }).save();
 
     await expect(saveGameWithSameId).to.be.eventually.rejected;
+  });
+
+  it("should save price information", async () => {
+    const g = await new Game({
+      nsuid: 21311,
+      name: "Zeldinha",
+      prices: [
+        {
+          location: "BR",
+          currency: "BRL",
+          originalPrice: 14,
+          price: 7,
+          discount: 0.5,
+          status: "available",
+          hasDiscount: true,
+        },
+        {
+          location: "AU",
+          currency: "AUD",
+          originalPrice: 24,
+          price: 2.4,
+          discount: 0.9,
+          status: "available",
+          hasDiscount: true,
+        },
+      ],
+    }).save();
+
+    expect(g.prices.filter((p) => p.currency === "BRL")).to.exist;
+    expect(g.prices.filter((p) => p.currency === "AUD")).to.exist;
+    expect(g.prices.length).to.eq(2);
+  });
+
+  it("should not save price with non-unique location-currency", async () => {
+    const g = new Game({
+      nsuid: 21311,
+      name: "Zeldinha",
+      prices: [
+        {
+          location: "BR",
+          currency: "BRL",
+          originalPrice: 14,
+          price: 7,
+          discount: 0.5,
+          status: "available",
+          hasDiscount: true,
+        },
+        {
+          location: "BR",
+          currency: "BRL",
+          originalPrice: 24,
+          price: 2.4,
+          discount: 0.9,
+          status: "available",
+          hasDiscount: true,
+        },
+      ],
+    }).save();
+
+    await expect(g).to.be.eventually.rejected;
+  });
+
+  it("should not update price with non-unique location-currency", async () => {
+    const g = await new Game({
+      nsuid: 21311,
+      name: "Zeldinha",
+      prices: [
+        {
+          location: "BR",
+          currency: "BRL",
+          originalPrice: 14,
+          price: 7,
+          discount: 0.5,
+          status: "available",
+          hasDiscount: true,
+        },
+      ],
+    }).saveAndFind();
+
+    g.prices.push({
+      location: "BR",
+      currency: "BRL",
+      originalPrice: 14 * 2,
+      price: 7 * 2,
+      discount: 0.5,
+      status: "available",
+      hasDiscount: true,
+    });
+
+    const updatePromise = g.save();
+
+    await expect(updatePromise).to.be.eventually.rejected;
+  });
+
+  it("should search for BRL prices lower than specified", async () => {
+    await new Game({
+      nsuid: 999,
+      name: "Zeldinha",
+      prices: [
+        {
+          location: "BR",
+          currency: "BRL",
+          originalPrice: 14,
+          price: 7,
+          discount: 0.5,
+          status: "available",
+          hasDiscount: true,
+        },
+        {
+          location: "AU",
+          currency: "AUD",
+          originalPrice: 24,
+          price: 2.4,
+          discount: 0.9,
+          status: "available",
+          hasDiscount: true,
+        },
+      ],
+    }).save();
+
+    await new Game({
+      nsuid: 2,
+      name: "Zeldinha2",
+      prices: [
+        {
+          location: "BR",
+          currency: "BRL",
+          originalPrice: 900,
+          price: 450,
+          discount: 0.5,
+          status: "available",
+          hasDiscount: true,
+        },
+      ],
+    }).save();
+
+    await new Game({
+      nsuid: 3,
+      name: "Zeldinha",
+      prices: [
+        {
+          location: "AU",
+          currency: "AUD",
+          originalPrice: 1,
+          price: 0.5,
+          discount: 0.5,
+          status: "available",
+          hasDiscount: true,
+        },
+      ],
+    }).save();
+
+    const games = await Game.find({
+      "prices.currency": "BRL",
+      "prices.price": {
+        $lt: 8,
+      },
+    })
+      .lean()
+      .exec();
+
+    expect(games.length).to.eq(1);
+    expect(games.pop()?.nsuid).to.eq(999);
   });
 });
--- src/data/mongo/Game.ts
+++ src/data/mongo/Game.ts
@@ -1,4 +1,7 @@
+/* eslint-disable no-var */
 import mongoose, { Schema, Document, Model } from "mongoose";
+
+const ModelName = "Game";
 
 export interface GameDocument extends Document {
   nsuid: number;
@@ -9,12 +12,15 @@ export interface GameDocument extends Document {
 
   prices: {
     location: string;
+    currency: string;
     originalPrice: number;
     price: number;
     discount: number;
     status: string;
     hasDiscount: boolean;
   }[];
+
+  saveAndFind: () => Promise<any>;
 }
 
 export const GameSchema = new Schema<GameDocument>({
@@ -40,6 +46,7 @@ export const GameSchema = new Schema<GameDocument>({
   prices: [
     {
       location: String,
+      currency: String,
       originalPrice: Number,
       price: Number,
       discount: Number,
@@ -49,7 +56,41 @@ export const GameSchema = new Schema<GameDocument>({
   ],
 });
 
+GameSchema.index({
+  "prices.location": 1,
+  "prices.currency": -1,
+});
+
+// eslint-disable-next-line func-names
+GameSchema.methods.saveAndFind = async function () {
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
+  const self = this;
+
+  const doc = await self.save();
+
+  // eslint-disable-next-line no-underscore-dangle
+  return self.model(ModelName).findById(doc._id);
+};
+
+GameSchema.pre("validate", function validate(next) {
+  const unique = [];
+
+  const self = this as any;
+
+  for (let i = 0, l = self.prices.length; i < l; i += 1) {
+    const { location, currency } = self.prices[i];
+    const key = `${location}_${currency}`;
+
+    if (unique.indexOf(key) > -1) {
+      return next(new Error("Duplicated price information"));
+    }
+    unique.push(key);
+  }
+
+  return next();
+});
+
 const defineMongooseModel = <TDocument extends Document>(schema: Schema<TDocument>, name: string): Model<TDocument> =>
   mongoose.model<TDocument>(name, schema);
 
-export const Game = defineMongooseModel(GameSchema, "Game");
+export const Game = defineMongooseModel(GameSchema, ModelName);
