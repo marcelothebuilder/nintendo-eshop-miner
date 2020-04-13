1b54fd67880eaed70ba4b54250c573e2f7d6e04b
Marcelo Paixão Resende <marcelothebuilder@users.noreply.github.com>
2020-04-13 01:41:45 -0300

    sanitize input games

--- src/integrate/sources/NorthAmericaIntegrationSource.ts
+++ src/integrate/sources/NorthAmericaIntegrationSource.ts
@@ -2,24 +2,45 @@ import { NorthAmericaDumper } from "../../dumpers/northamerica/NorthAmericaDumpe
 import { IntegrationSource, IntegrationGame } from "../IntegrationSource";
 import { NorthAmericaGame } from "../../dumpers/northamerica/NorthAmericaGame";
 import { Region } from "../../data/mongo/Game";
+import { logger } from "../../logging/logger";
 
-const convertGame = (game: NorthAmericaGame): IntegrationGame => ({
-  title: game.title,
-  nsuid: parseInt(game.nsuid, 10),
-  slug: game.slug,
-  imageUrl: game.boxArt,
-  description: game.description,
-  sortingName: game.title,
-  location: "us",
-  categories: game.categories,
-  publishers: game.publishers,
-  releaseDate: new Date(game.releaseDateMask),
-  remoteLastModified: new Date(game.lastModified),
-  region: Region.America,
-});
+const convertGame = (game: NorthAmericaGame): IntegrationGame => {
+  const nsuid = parseInt(game.nsuid, 10);
+  if (Number.isNaN(nsuid)) {
+    throw Error(`Invalid property nsuid`);
+  }
+
+  if (!game.slug) {
+    throw Error(`Invalid property slug`);
+  }
+
+  return {
+    title: game.title,
+    nsuid,
+    slug: game.slug,
+    imageUrl: game.boxArt,
+    description: game.description,
+    sortingName: game.title,
+    location: "us",
+    categories: game.categories,
+    publishers: game.publishers,
+    releaseDate: new Date(game.releaseDateMask),
+    remoteLastModified: new Date(game.lastModified),
+    region: Region.America,
+  };
+};
 
 // eslint-disable-next-line func-names
 export const NorthAmericaIntegrationSource = async function* (dumper: NorthAmericaDumper): IntegrationSource {
   const result = await dumper.searchAll();
-  yield result.games.map(convertGame);
+  yield result.games
+    .map((game) => {
+      try {
+        return convertGame(game);
+      } catch (e) {
+        logger.error(`Skipping game ${game.title} because of ${e.message}`);
+        return null;
+      }
+    })
+    .filter((g) => !!g) as IntegrationGame[];
 };
